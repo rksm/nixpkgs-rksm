b622751988044e438097f6e322f9627d0ed15862
Robert Krahn <robert.krahn@gmail.com>
2025-01-21 14:13:12 +0100

    fixit

--- default.nix
+++ default.nix
@@ -1,5 +1,7 @@
-{ pkgs }:
-
+{ system, nixpkgs }:
+let
+  pkgs = import nixpkgs { inherit system; };
+in
 {
   uniclip = pkgs.callPackage ./uniclip.nix { };
 }
--- flake.nix
+++ flake.nix
@@ -7,9 +7,7 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
-        packages = import ./. { inherit pkgs; };
-
+        packages = import ./. { inherit system nixpkgs; };
       in
       {
         inherit packages;
