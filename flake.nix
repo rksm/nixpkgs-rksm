1e381e97f8b2fcd636f37e5d618192fe188a9371
Robert Krahn <robert.krahn@gmail.com>
2025-01-21 14:30:27 +0100

    keep flake-utils

--- flake.nix
+++ flake.nix
@@ -1,26 +1,16 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      supported-systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-    in
-
-    {
-      packages = builtins.listToAttrs (map
-        (system:
-          {
-            name = system;
-            value = import ./. { inherit system nixpkgs; };
-          }
-        )
-        supported-systems);
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        packages = import ./. { inherit system nixpkgs; };
+      in
+      {
+        inherit packages;
+      }
+    );
 }
